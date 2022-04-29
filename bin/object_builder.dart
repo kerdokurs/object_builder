9bf5b0a2c329014f8d7bff4e9a2f229ffade6ed7
Kerdo Kurs <kerdokurs@gmail.com>
2022-04-29 19:13:38 +0300

    initial commit

---
+++ bin/object_builder.dart
@@ -0,0 +1,5 @@
+import 'package:object_builder/object_builder.dart' as object_builder;
+
+void main(List<String> arguments) {
+  print('Hello world: ${object_builder.calculate()}!');
+}
---
+++ test/object_builder_test.dart
@@ -0,0 +1,8 @@
+import 'package:object_builder/object_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
